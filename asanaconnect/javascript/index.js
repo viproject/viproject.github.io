79d89092cfe0afee60f2cf9cbb4a634d10f891ba
Dmitrij Burakov <dmitrij@viproject.co.uk>
2021-02-10 14:32:39 +0000

    commit

--- asanaconnect/javascript/index.js
+++ asanaconnect/javascript/index.js
@@ -4,7 +4,8 @@ window.onload = function() {
     document.getElementById('date').innerHTML = new Date().toDateString();
     
     var node;
-    if(true) {
+    const asana_code = getAsanaCode();
+    if(asana_code) {
         node = document.getElementById('if-part');
     }
     else {
@@ -12,5 +13,16 @@ window.onload = function() {
     }
     node.style.visibility = 'visible';
 }
+
+function getAsanaCode () {
+    
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const code_asana = urlParams.get('code');
+    return code_asana;
+}
+
+
+
 
 
